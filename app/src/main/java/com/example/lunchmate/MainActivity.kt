b682718e5384dda2e263dba77f1ed09271fa5dd2
Karina <karina-samsonova-2002@mail.ru>
2024-04-04 18:04:27 +0300

    Added badges to bottomNavigation

--- app/src/main/java/com/example/lunchmate/MainActivity.kt
+++ app/src/main/java/com/example/lunchmate/MainActivity.kt
@@ -3,30 +3,43 @@ package com.example.lunchmate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.lunchmate.databinding.ActivityMainBinding
+import com.example.lunchmate.databinding.FragmentAccountEditBinding
 import com.example.lunchmatelocal.Account
 import com.example.lunchmatelocal.AccountFragment
 import com.example.lunchmatelocal.HomeFragment
+import com.google.android.material.badge.BadgeDrawable
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var currentUser: Account
     val offices = arrayOf("Офис 1", "Офис 2", "Офис 3")
+    var badge_counter = 13
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val homeFragment = HomeFragment()
         val profileFragment = AccountFragment()
-
         setCurrentFragment(homeFragment)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNav.setOnItemSelectedListener {
+        updateBadge()
+        binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home->setCurrentFragment(homeFragment)
                 R.id.profile->setCurrentFragment(profileFragment)
+                R.id.notifications->{badge_counter = 0
+                    updateBadge()}
+                R.id.schedule-> {
+                    if (binding.bottomNavigationView.selectedItemId != R.id.notifications) {
+                        badge_counter++
+                        updateBadge()
+                    }
+                }
             }
             true
         }
@@ -40,4 +53,19 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
 
+    fun updateBadge(){
+        if (badge_counter == 0){
+            binding.bottomNavigationView.getBadge(R.id.notifications)?.isVisible = false
+            binding.bottomNavigationView.removeBadge(R.id.notifications)
+        }
+        else {
+            binding.bottomNavigationView.getOrCreateBadge(R.id.notifications).apply{
+                isVisible = true
+                number = badge_counter
+                backgroundColor = getResources().getColor(R.color.yellow_600)
+                badgeTextColor = getResources().getColor(R.color.black)
+                maxCharacterCount = 3
+            }
+        }
+    }
 }
