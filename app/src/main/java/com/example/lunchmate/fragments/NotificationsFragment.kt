b4d74cb978a32d9d6018d09cc46899664dc58af0
Karina <karina-samsonova-2002@mail.ru>
2024-04-08 15:43:28 +0300

    Added delete on swipe for notifications

--- app/src/main/java/com/example/lunchmate/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/lunchmate/fragments/NotificationsFragment.kt
@@ -4,21 +4,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lunchmate.MainActivity
 import com.example.lunchmate.R
 import com.example.lunchmate.adapters.NotificationsAdapter
-import com.example.lunchmate.adapters.SlotsAdapter
 import com.example.lunchmate.databinding.FragmentNotificationsBinding
 import com.example.lunchmate.model.Notification
-import com.example.lunchmatelocal.Account
 import com.example.lunchmatelocal.AvailableSlotsAdapter
-import com.example.lunchmatelocal.ProfilesAdapter
 import com.example.lunchmatelocal.Slot
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 
 class NotificationsFragment: Fragment(R.layout.fragment_notifications) {
     private lateinit var binding: FragmentNotificationsBinding
@@ -33,6 +33,8 @@ class NotificationsFragment: Fragment(R.layout.fragment_notifications) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNotificationsBinding.bind(view)
         val activity = activity as MainActivity
+        activity.badge_counter = 0
+        activity.updateBadge()
 
         notificationsList = ArrayList<Notification>()
         notificationsList.add(Notification("Новое приглашение", Slot("1 марта", "11:00", "12:00"), activity.currentUser))
@@ -92,5 +94,44 @@ class NotificationsFragment: Fragment(R.layout.fragment_notifications) {
         val linearLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.recyclerView.layoutManager = linearLayoutManager
         binding.recyclerView.adapter = notificationsAdapter
+        binding.recyclerView.itemAnimator = SlideInLeftAnimator()
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val notification: Notification =
+                    notificationsList.get(position)
+                notificationsList.removeAt(position)
+                notificationsAdapter.notifyItemRemoved(position)
+                val snackBar = Snackbar.make(requireView(), "Уведомление удалено", Snackbar.LENGTH_LONG)
+                    .setTextColor(ContextCompat.getColor(
+                        requireContext(), R.color.white
+                    ))
+                    .setActionTextColor(
+                        ContextCompat.getColor(
+                            requireContext(), R.color.yellow_600
+                        ))
+                    .setAction(
+                        "Отменить",
+                        View.OnClickListener {
+                            notificationsList.add(position, notification)
+                            notificationsAdapter.notifyItemInserted(position)
+                        }).show()
+            }
+        }).attachToRecyclerView(binding.recyclerView)
+
+        binding.clearBtn.setOnClickListener {
+            val count = notificationsList.size
+            notificationsList.clear()
+            notificationsAdapter.notifyItemRangeRemoved(0, count)
+        }
     }
 }
